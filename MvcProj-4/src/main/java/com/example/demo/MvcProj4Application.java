6de61f3a4d26b5d9d5d32d60624fe5345d08f484
megha <meghabhagwati676@gmail.com>
2021-06-10 18:27:27 +0530

    all

---
+++ MvcProj-4/src/main/java/com/example/demo/MvcProj4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcProj4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcProj4Application.class, args);
+	}
+
+}
